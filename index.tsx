e13057e88223d143252cb33f02125b0c026c53fe
Monk <1710.mahajan@gmail.com>
2025-12-19 19:04:05 +0530

    hj

--- index.tsx
+++ index.tsx
@@ -1,16 +1,3 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is intentionaly empty to support a Vanilla JS project structure
+// within the existing preview environment constraints.
